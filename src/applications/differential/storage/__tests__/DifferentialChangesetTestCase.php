f3ec196d0f6f9e6faf286436e3427f8d1df29a8c
Manuel Klimek <klimek@google.com>
2012-08-10 21:57:59 +0000

    First passing tests.

--- src/applications/differential/storage/__tests__/DifferentialChangesetTestCase.php
+++ src/applications/differential/storage/__tests__/DifferentialChangesetTestCase.php
@@ -18,22 +18,42 @@ final class DifferentialChangeSetTestCase extends PhabricatorTestCase {
   // $length: 0 based (0 meaning 1 line)
   private function createOldComment($line, $length) {
     $comment = $this->createComment();
-    $comment->setIsNewFile(True);
+    $comment->setIsNewFile(False);
     $comment->setLineNumber($line);
     $comment->setLineLength($length);
     return $comment;
   }
+  private function createHunk($oldOffset, $oldLen, $newOffset, $newLen, $changes) {
+    $hunk = new DifferentialHunk();
+    $hunk->setOldOffset($oldOffset);
+    $hunk->setOldLen($oldLen);
+    $hunk->setNewOffset($newOffset);
+    $hunk->setNewLen($newLen);
+    $hunk->setChanges($changes);
+    return $hunk;
+  }
   private function createChange($hunks) {
     $change = new DifferentialChangeset();
-    $change->setHunks($hunks);
+    $change->attachHunks($hunks);
+    return $change;
   }
-  public function testFail() {
+
+  public function testOneLineOldComment() {
     $change = $this->createChange(array(
       0 => $this->createHunk(1, 1, 0, 0, "-a"),
     ));
+    $comment = $this->createOldComment(1, 0); 
+
+    $this->assertEqual("@@ -1,1 @@\n-a", $change->makeContextDiff($comment));
+  }
+
+  public function testOneLineNewComment() {
+    $change = $this->createChange(array(
+      0 => $this->createHunk(0, 0, 1, 1, "+a"),
+    ));
     $comment = $this->createNewComment(1, 0); 
 
-    $this->assertEqual("a", "a");
+    $this->assertEqual("@@ +1,1 @@\n+a", $change->makeContextDiff($comment));
   }
 /*
   public function testMakeChanges() {
