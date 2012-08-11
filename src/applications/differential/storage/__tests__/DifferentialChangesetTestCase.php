a0c26cc3aa8b1b7fb2d7b56f79ecad92ade20633
Manuel Klimek <klimek@google.com>
2012-08-11 08:28:40 +0000

    Get rid of copied test.

--- src/applications/differential/storage/__tests__/DifferentialChangesetTestCase.php
+++ src/applications/differential/storage/__tests__/DifferentialChangesetTestCase.php
@@ -55,38 +55,5 @@ final class DifferentialChangeSetTestCase extends PhabricatorTestCase {
 
     $this->assertEqual("@@ +1,1 @@\n+a", $change->makeContextDiff($comment));
   }
-/*
-  public function testMakeChanges() {
-    $root = dirname(__FILE__).'/hunk/';
-
-    $hunk = new DifferentialHunk();
-    $hunk->setChanges(Filesystem::readFile($root.'basic.diff'));
-    $hunk->setOldOffset(1);
-    $hunk->setNewOffset(11);
-
-    $old = Filesystem::readFile($root.'old.txt');
-    $this->assertEqual($old, $hunk->makeOldFile());
-
-    $new = Filesystem::readFile($root.'new.txt');
-    $this->assertEqual($new, $hunk->makeNewFile());
-
-    $added = array(
-      12 => "1 quack\n",
-      13 => "1 quack\n",
-      16 => "5 drake\n",
-    );
-    $this->assertEqual($added, $hunk->getAddedLines());
-
-    $hunk = new DifferentialHunk();
-    $hunk->setChanges(Filesystem::readFile($root.'newline.diff'));
-    $hunk->setOldOffset(1);
-    $hunk->setNewOffset(11);
-
-    $this->assertEqual("a\n", $hunk->makeOldFile());
-    $this->assertEqual("a", $hunk->makeNewFile());
-    $this->assertEqual(array(11 => "a"), $hunk->getAddedLines());
-
-  }
-*/
 }
 
